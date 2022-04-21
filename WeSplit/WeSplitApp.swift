0ac96258cf4c61a27cd91abb42b53679910d0e37
HexaTeam6 <abdurrachmanwahed@gmail.com>
2022-04-21 23:31:26 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Abdur Rachman Wahed on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
